289e119ad625fc998420a73647b8ba93ba4a04ef
Zach <zacharygrigg1@gmail.com>
2020-08-14 20:10:45 -0500

    [JAST-44] Write frost integration code

--- src/main/java/mpq/MpqEditor.java
+++ src/main/java/mpq/MpqEditor.java
@@ -1,11 +1,16 @@
 package mpq;
 
 import constants.Constants;
+import frost.FrostMpq;
 import helper.FileHelper;
 import helper.InterfaceHelper;
+import interfaces.IFrostMpq;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import settings.MpqSettings;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +21,7 @@ import java.util.List;
 public class MpqEditor {
 
     private File file;
+    private IFrostMpq frostMpq;
     private static final String[] FILES = {"war3map.j", "scripts\\war3map.j", "war3map.wts", "war3map.w3u", "war3map.w3t", "war3map.w3a"};
 
     /**
@@ -25,15 +31,13 @@ public class MpqEditor {
      */
     public MpqEditor(File file) {
         this.file = file;
-    }
+        MpqSettings settings = new MpqSettings();
+        settings.setCompressionSettings(MpqSettings.CompressionSettings.DEFLATE);
+        settings.setLogSettings(MpqSettings.LogSettings.INFO);
+        settings.setMpqOpenSettings(MpqSettings.MpqOpenSettings.CRITICAL);
+        settings.setSecuritySettings(MpqSettings.SecuritySettings.ENCRYPTION_DISABLED);
 
-    private void copyFileToMpqDir() {
-        try {
-            FileUtils.copyFile(this.file, new File(Constants.FROZENMPQ_FOLDER + "myMap.w3x"));
-            System.out.println("Copied file to mpq directory");
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
-        }
+        frostMpq = new FrostMpq(file);
     }
 
     /**
@@ -49,25 +53,27 @@ public class MpqEditor {
             throw new RuntimeException("Not a directory: " + destinationFolder.getName());
         }
         File output = new File(Constants.FROZENMPQ_OUTPUT);
-        if(output.exists()) {
-            if(!output.isDirectory()) {
+        if (output.exists()) {
+            if (!output.isDirectory()) {
                 output.delete();
             } else {
                 FileHelper.deleteDirectory(output);
             }
         }
-        copyFileToMpqDir();
-        StringBuilder fileCommand = new StringBuilder();
-        for (String file : FILES) {
-            fileCommand.append("\"")
-                    .append(file).append("\" ");
+
+
+        for (String fileName : FILES) {
+            try {
+                if (frostMpq.fileExists(fileName)) {
+                    System.out.println("Extracting: " + fileName);
+                    frostMpq.extractFile(fileName);
+                }
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
         }
-        fileCommand.setLength(fileCommand.length()-1);
-        System.out.println(
-                executeFrozenMpq("\"ext\"",
-                        "\"" + Constants.USER_PATH +
-                                Constants.FROZENMPQ_FOLDER +
-                                "myMap.w3x\"", fileCommand.toString()));
+
+
         try {
             FileUtils.copyDirectory(output, destinationFolder);
             FileUtils.deleteDirectory(output);
@@ -83,46 +89,35 @@ public class MpqEditor {
      *
      * @param directory Directory containing files to import
      */
-    public void packFiles(File directory, File destinationFile) {
-        List<File> files = new ArrayList<>();
-        List<File> directories = new ArrayList<>();
+    public void packFiles(File directory, File destination) {
         File input = new File(Constants.FROZENMPQ_INPUT);
-        if(input.exists()) {
-            if(!input.isDirectory()) {
+        if (input.exists()) {
+            if (!input.isDirectory()) {
                 input.delete();
             } else {
                 FileHelper.deleteDirectory(input);
             }
         }
         input.mkdirs();
-        copyFileToMpqDir();
         try {
             FileUtils.copyDirectory(directory, input);
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         }
-        FileHelper.recursiveFolderDiscovery(directory, directories, files);
-        StringBuilder fileCommand = new StringBuilder();
-        for (String file : FILES) {
-            fileCommand.append("\"")
-                    .append(file).append("\" ");
-        }
-        fileCommand.setLength(fileCommand.length()-1);
-
-        System.out.println(
-                executeFrozenMpq("\"imp\"",
-                        "\"" + Constants.USER_PATH +
-                                Constants.FROZENMPQ_FOLDER +
-                                "myMap.w3x\"", fileCommand.toString()));
 
         try {
+            for (String fileName : FILES) {
+                File desiredFile = new File(fileName);
+                if (desiredFile.exists()) {
+                    frostMpq.importFile(fileName, IOUtils.toByteArray(new FileInputStream(desiredFile)));
+                }
+            }
+
+            frostMpq.save(destination);
+
             FileUtils.deleteDirectory(input);
-            FileUtils.copyFile(
-                    new File(Constants.FROZENMPQ_FOLDER +  "myMap.w3x"),
-                    destinationFile);
-            new File(Constants.FROZENMPQ_FOLDER +  "myMap.w3x").delete();
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
         }
     }
 
